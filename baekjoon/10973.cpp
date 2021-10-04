ea325f9756414e744b357a1bfff9374c7352c22e
mminos <9911beta@gmail.com>
2021-10-04 15:49:27 +0900

    21/10/04 algo

---
+++ baekjoon/10973.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[10000];
+
+int main(void) {
+	int n;
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	bool res = prev_permutation(arr, arr + n);
+
+	if (res) {
+		for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+	}
+	else printf("-1");
+
+	return 0;
+}
